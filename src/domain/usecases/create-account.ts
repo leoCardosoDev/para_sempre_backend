9299e7b974a14df683410241a0e463203c304149
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-09 09:37:56 -0700

    refactor: add CreateAccount with model domain again

--- src/domain/usecases/create-account.ts
+++ src/domain/usecases/create-account.ts
@@ -1,4 +1,5 @@
 import { CreateAccountParams, CreateAccountResult } from '@/domain/models'
+
 export interface CreateAccount {
   create: (_account: CreateAccountParams) => Promise<CreateAccountResult>
 }
