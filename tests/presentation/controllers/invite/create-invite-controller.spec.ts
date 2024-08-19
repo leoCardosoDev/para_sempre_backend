3cd3104bad9e179fd22fc138ad15cb0981d37d96
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-19 14:01:10 -0700

    test: ensure CreateInviteController calls Create invite with correct values

--- tests/presentation/controllers/invite/create-invite-controller.spec.ts
+++ tests/presentation/controllers/invite/create-invite-controller.spec.ts
@@ -1,35 +1,39 @@
+import { CreateInvite } from '@/domain/usecases/invite'
 import { CreateInviteController, CreateInviteControllerParams } from '@/presentation/controllers/invite'
 import { MissingParamError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers'
-import { ValidationSpy } from '@/tests/presentation/mocks'
+import { CreateInviteSpy, ValidationSpy } from '@/tests/presentation/mocks'
 
 import { faker } from '@faker-js/faker'
 
-const mockRequest = (): CreateInviteControllerParams => ({
-    invite_id: faker.string.uuid(),
-    admin_id: faker.string.uuid(),
-    invite_code: faker.string.uuid(),
-    email_user: faker.internet.email(),
-    phone_user: faker.string.numeric({ length: { min: 10, max: 12 }}),
+const mockRequest = (expirationDate?: Date): CreateInviteControllerParams => ({
+    inviteId: faker.string.uuid(),
+    adminId: faker.string.uuid(),
+    inviteCode: faker.string.uuid(),
+    emailUser: faker.internet.email(),
+    phoneUser: faker.string.numeric({ length: { min: 10, max: 12 }}),
     status: faker.word.sample(),
-    invite_type: faker.word.sample(),
-    created_at: faker.date.recent(),
-    expiration: faker.date.future(),
-    used_at: null,
-    max_uses: faker.number.int({min: 0, max: 1})
+    inviteType: faker.word.sample(),
+    createdAt: faker.date.recent(),
+    expiration: expirationDate || faker.date.future(),
+    usedAt: null,
+    maxUses: faker.number.int({min: 0, max: 1})
 })
 
 type SutTypes = {
   sut: CreateInviteController,
-  validationSpy: ValidationSpy
+  validationSpy: ValidationSpy,
+  createInviteSpy: CreateInvite
 }
 
 const makeSut = (): SutTypes => {
   const validationSpy = new ValidationSpy()
-  const sut = new CreateInviteController(validationSpy)
+  const createInviteSpy = new CreateInviteSpy()
+  const sut = new CreateInviteController(validationSpy, createInviteSpy)
   return {
     sut,
-    validationSpy
+    validationSpy,
+    createInviteSpy
   }
 }
 
@@ -47,5 +51,13 @@ describe('CreateInvite Controller', () => {
     const request = mockRequest()
     const httpResponse = await sut.handle(request)
     expect(httpResponse).toEqual(badRequest(validationSpy.error))
+  })
+
+  it('should call CreateInvite with correct values', async () => {
+    const { sut, createInviteSpy } = makeSut()
+    const createSpy = jest.spyOn(createInviteSpy, 'create')
+    const request = mockRequest()
+    await sut.handle(request)
+    expect(createSpy).toHaveBeenCalledWith({...request, usedAt: null})
   })
 })
