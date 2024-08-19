fbaa43468109a47e5ceffcfdb4a9ec40a1086256
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-19 10:51:38 -0700

    feat: add types to invitecontroller

--- src/presentation/controllers/invite/create-invite-controller.ts
+++ src/presentation/controllers/invite/create-invite-controller.ts
@@ -2,8 +2,22 @@ import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class CreateInviteController implements Controller {
   constructor(private readonly _validation: Validation) {}
-  async handle(_request: any): Promise<HttpResponse> {
-    this._validation.validate(_request.body)
+  async handle(request: CreateInviteControllerParams): Promise<HttpResponse> {
+    this._validation.validate(request)
     return new Promise(resolve => resolve({ body: {}, statusCode: 0 }))
   }
+}
+
+export type CreateInviteControllerParams = {
+  invite_id: string
+  admin_id: string
+  invite_code: string
+  email_user: string
+  phone_user: string
+  status: string
+  invite_type: string
+  created_at: Date
+  expiration: Date
+  used_at: string | null
+  max_uses: number
 }
