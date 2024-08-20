e1048d434d0d71c535ab82d9212ad0afa31e1a5a
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-20 14:24:10 -0700

    feat: ensure AuthMiddleware calls LoadAccountRepoistory with correct role

--- src/main/factories/usecases/index.ts
+++ src/main/factories/usecases/index.ts
@@ -1,2 +1,3 @@
 export * from './authentication-factory'
 export * from './create-account-factory'
+export * from './load-account-by-token-factory'
--- src/main/routes/invite-routes.ts
+++ src/main/routes/invite-routes.ts
@@ -2,7 +2,8 @@ import { adaptRoute } from '@/main/adapters'
 
 import { Router } from 'express'
 import { makeCreateInviteController } from '../factories/controllers/invite'
+import { adminAuth } from '@/main/middlewares'
 
 export default (router: Router): void => {
-  router.post('/invites', adaptRoute(makeCreateInviteController()))
+  router.post('/invites', adminAuth, adaptRoute(makeCreateInviteController()))
 }
