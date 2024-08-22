b371b793f21bc7b570b9d6d47426da257dccb6da
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-21 18:52:20 -0700

    test: move SUT creation to a factory method

--- tests/application/usecases/invite/db-load-invite-by-code.spec.ts
+++ tests/application/usecases/invite/db-load-invite-by-code.spec.ts
@@ -1,11 +1,31 @@
 import { DbLoadInviteByCode } from '@/application/usecases/invite'
 import { DecrypterSpy } from '@/tests/application/mocks'
+import { faker } from '@faker-js/faker'
+
+type SutTypes = {
+  sut: DbLoadInviteByCode
+  decrypterSpy: DecrypterSpy
+}
+
+const makeSut = (): SutTypes => {
+  const decrypterSpy = new DecrypterSpy()
+  const sut = new DbLoadInviteByCode(decrypterSpy)
+  return {
+    sut,
+    decrypterSpy
+  }
+}
+
+let encryptedCode: string
 
 describe('DbLoadInviteByCode Usecases', () => {
+
+  beforeAll(() => {
+    encryptedCode = faker.string.uuid()
+  })
+
   it('should call Decrypter with correct ciphertext', async () => {
-    const decrypterSpy = new DecrypterSpy()
-    const sut = new DbLoadInviteByCode(decrypterSpy)
-    const encryptedCode = 'any_encrypted_code'
+    const { sut, decrypterSpy } = makeSut()
     await sut.load({ inviteCode: encryptedCode })
     expect(decrypterSpy.ciphertext).toBe(encryptedCode)
   })
