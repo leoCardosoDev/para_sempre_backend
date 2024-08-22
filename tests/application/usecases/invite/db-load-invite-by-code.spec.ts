a770c6d24b7270e77d43c33e09484812cef06fe6
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-21 19:47:55 -0700

    test: ensure DbLoadInvitebyCode returns invite on success

--- tests/application/usecases/invite/db-load-invite-by-code.spec.ts
+++ tests/application/usecases/invite/db-load-invite-by-code.spec.ts
@@ -1,20 +1,18 @@
 import { DbLoadInviteByCode } from '@/application/usecases/invite'
-import { DecrypterSpy, LoadInviteByCodeRepositorySpy } from '@/tests/application/mocks'
+import { LoadInviteByCodeRepositorySpy } from '@/tests/application/mocks'
+
 import { faker } from '@faker-js/faker'
 
 type SutTypes = {
   sut: DbLoadInviteByCode
-  decrypterSpy: DecrypterSpy
   loadInviteByCodeRepositorySpy: LoadInviteByCodeRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
-  const decrypterSpy = new DecrypterSpy()
   const loadInviteByCodeRepositorySpy = new LoadInviteByCodeRepositorySpy
-  const sut = new DbLoadInviteByCode(decrypterSpy, loadInviteByCodeRepositorySpy)
+  const sut = new DbLoadInviteByCode(loadInviteByCodeRepositorySpy)
   return {
     sut,
-    decrypterSpy,
     loadInviteByCodeRepositorySpy
   }
 }
@@ -27,30 +25,23 @@ describe('DbLoadInviteByCode Usecases', () => {
     encryptedCode = faker.string.uuid()
   })
 
-  it('should call Decrypter with correct ciphertext', async () => {
-    const { sut, decrypterSpy } = makeSut()
-    await sut.load({ inviteCode: encryptedCode })
-    expect(decrypterSpy.ciphertext).toBe(encryptedCode)
-  })
-
-  it('should returns null if Decrypter null', async () => {
-    const { sut, decrypterSpy } = makeSut()
-    decrypterSpy.decrypt = jest.fn().mockResolvedValueOnce(null)
-    const invite = await sut.load({ inviteCode: encryptedCode })
-    expect(invite).toBeNull()
-  })
-
   it('should call LoadInviteByCodeRepository with correct values with correct ciphertext', async () => {
     const { sut, loadInviteByCodeRepositorySpy } = makeSut()
     await sut.load({ inviteCode: encryptedCode })
     expect(loadInviteByCodeRepositorySpy.inviteCode).toBe(encryptedCode)
   })
 
-  test('Should return null if LoadInviteByCodeRepository returns null', async () => {
+  it('Should return null if LoadInviteByCodeRepository returns null', async () => {
     const { sut, loadInviteByCodeRepositorySpy } = makeSut()
     loadInviteByCodeRepositorySpy.loadByCode = jest.fn().mockResolvedValueOnce(null)
     const invite = await sut.load({ inviteCode: encryptedCode })
     expect(invite).toBeNull()
+  })
+
+  it('Should return an invite on success', async () => {
+    const { sut, loadInviteByCodeRepositorySpy } = makeSut()
+    const invite = await sut.load({ inviteCode: encryptedCode })
+    expect(invite).toEqual(loadInviteByCodeRepositorySpy.result)
   })
 })
 
