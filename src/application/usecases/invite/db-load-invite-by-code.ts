24fa089a9722b069a46180db9975843208cdf0fb
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-21 19:47:16 -0700

    feat: ensure DbLoadInvitebyCode returns invite on success

--- src/application/usecases/invite/db-load-invite-by-code.ts
+++ src/application/usecases/invite/db-load-invite-by-code.ts
@@ -1,16 +1,11 @@
-import { Decrypter } from '@/application/protocols'
 import { LoadInviteByCodeRepository } from '@/application/protocols/db/invite'
 import { LoadInviteByCode, LoadInviteByCodeParams, LoadInviteByCodeResult } from '@/domain/usecases/invite'
 
 export class DbLoadInviteByCode implements LoadInviteByCode {
-  constructor(
-    private readonly _decrypter: Decrypter,
-    private readonly _loadByCodeRepository: LoadInviteByCodeRepository
-  ) {}
+  constructor(private readonly _loadByCodeRepository: LoadInviteByCodeRepository) {}
 
   async load(_params: LoadInviteByCodeParams): Promise<LoadInviteByCodeResult> {
-    await this._decrypter.decrypt(_params.inviteCode)
-    await this._loadByCodeRepository.loadByCode(_params)
-    return null
+    const inviteData = await this._loadByCodeRepository.loadByCode(_params)
+    return inviteData
   }
 }
