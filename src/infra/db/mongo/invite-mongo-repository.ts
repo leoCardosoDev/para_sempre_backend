2563cf7183b5fa8a0c01abcd3f02f774ef9600f5
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-22 09:29:55 -0700

    feat: ensure InviteMongoRepository returns data on success

--- src/infra/db/mongo/invite-mongo-repository.ts
+++ src/infra/db/mongo/invite-mongo-repository.ts
@@ -1,12 +1,31 @@
-import { CreateInviteRepository } from '@/application/protocols/db/invite'
+import { CreateInviteRepository, LoadInviteByCodeRepository, LoadInviteByCodeRepositoryResult } from '@/application/protocols/db/invite'
 import { CreateInviteParams, CreateInviteResult } from '@/domain/usecases/invite'
 import { MongoHelper } from '@/infra/db/mongo'
 
-export class InviteMongoRepository implements CreateInviteRepository {
+export class InviteMongoRepository implements CreateInviteRepository, LoadInviteByCodeRepository {
   async create(_inviteData: CreateInviteParams): Promise<CreateInviteResult> {
     const inviteCollection = await MongoHelper.getCollection('invites')
     const result = await inviteCollection.insertOne(_inviteData)
     const inviteWithId = { ..._inviteData, _id: result.insertedId }
     return MongoHelper.map(inviteWithId)
+  }
+
+  async loadByCode(inviteCode: string): Promise<LoadInviteByCodeRepositoryResult> {
+    const inviteCollection = await MongoHelper.getCollection('invites')
+    const invite = await inviteCollection.findOne(
+      {
+        inviteCode
+      },
+      {
+        projection: {
+          _id: 1,
+          emailUser: 1,
+          phoneUser: 1,
+          status: 1,
+          expiration: 1
+        }
+      }
+    )
+    return invite && MongoHelper.map(invite)
   }
 }
