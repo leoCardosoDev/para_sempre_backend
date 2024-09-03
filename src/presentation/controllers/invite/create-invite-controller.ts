cde6202cc62329e0f720ef1468331293e3333bd6
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-03 11:20:36 -0700

    feat: ensure CreateInviteController returns 400 if validation fails

--- src/presentation/controllers/invite/create-invite-controller.ts
+++ src/presentation/controllers/invite/create-invite-controller.ts
@@ -1,10 +1,12 @@
+import { badRequest } from '@/presentation/helpers'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class CreateInviteController implements Controller {
   constructor(private readonly _validation: Validation) {}
 
   async handle(request: any): Promise<HttpResponse> {
-    this._validation.validate(request)
+    const error = this._validation.validate(request)
+    if (error) return badRequest(error)
     return new Promise(resolve => resolve({ statusCode: 200, body: {} })) //temporario até terminar a feature
   }
 }
