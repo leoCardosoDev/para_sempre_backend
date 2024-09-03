d7f98eadeffcb695f320b8d67e7b047b4858e561
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-02 18:55:40 -0700

    test: ensure DbCreateInvite calls InviteCodeGenerator with correct values

--- tests/application/usecases/invite/db-create-invite.spec.ts
+++ tests/application/usecases/invite/db-create-invite.spec.ts
@@ -1,11 +1,11 @@
 import { CreateInviteParams } from '@/domain/usecases/invite'
 import { DbCreateInvite } from '@/application/usecases/invite'
-import { CreateInviteRepositorySpy } from '@/tests/application/mocks'
+import { CreateInviteRepositorySpy, InviteCodeGeneratorSpy } from '@/tests/application/mocks'
 import { faker } from '@faker-js/faker'
 
 const mockInviteData = (): CreateInviteParams => ({
   accountId: faker.string.uuid(),
-  inviteCode: faker.string.uuid(),
+  inviteCode: 'unique_invite_code',
   emailUser: faker.internet.email(),
   phoneUser: faker.string.numeric({ length: { min: 10, max: 12 } }),
   status: faker.word.sample(),
@@ -19,25 +19,43 @@ const mockInviteData = (): CreateInviteParams => ({
 type SutTypes = {
   sut: DbCreateInvite
   createInviteRepositorySpy: CreateInviteRepositorySpy
+  inviteCodeGenaratorSpy: InviteCodeGeneratorSpy
 }
 
 const makeSut = (): SutTypes => {
   const createInviteRepositorySpy = new CreateInviteRepositorySpy()
-  const sut = new DbCreateInvite(createInviteRepositorySpy)
+  const inviteCodeGenaratorSpy = new InviteCodeGeneratorSpy()
+  const sut = new DbCreateInvite(createInviteRepositorySpy, inviteCodeGenaratorSpy)
   return {
     sut,
-    createInviteRepositorySpy
+    createInviteRepositorySpy,
+    inviteCodeGenaratorSpy
   }
 }
 
 describe('DbCreateInvite Usecase', () => {
+  it('should generate a unique inviteCode and pass it to CreateInviteRepository', async () => {
+    const { sut, inviteCodeGenaratorSpy, createInviteRepositorySpy } = makeSut()
+    const inviteCode = 'unique_invite_code'
+    inviteCodeGenaratorSpy.generate.mockResolvedValue(inviteCode)
+    const inviteData = mockInviteData()
+    const expectedInviteData = { ...inviteData, inviteCode }
+    const createSpy = jest.spyOn(createInviteRepositorySpy, 'createInvite')
+    await sut.create(inviteData)
+    expect(inviteCodeGenaratorSpy.generate).toHaveBeenCalledTimes(1)
+    expect(createSpy).toHaveBeenCalledWith(expectedInviteData)
+  })
+
   it('should call CreateInviteRepository with correct values', async () => {
-    const { sut, createInviteRepositorySpy } = makeSut()
+    const { sut, createInviteRepositorySpy, inviteCodeGenaratorSpy } = makeSut()
+    const inviteCode = 'unique_invite_code'
+    inviteCodeGenaratorSpy.generate.mockResolvedValue(inviteCode)
     const createSpy = jest.spyOn(createInviteRepositorySpy, 'createInvite')
     const inviteData = mockInviteData()
     await sut.create(inviteData)
     expect(createSpy).toHaveBeenCalledWith({
-      ...inviteData
+      ...inviteData,
+      inviteCode
     })
   })
 })
