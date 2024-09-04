514e0023049c408cb87d16d6a5689ef6930fc68a
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-04 08:41:32 -0700

    feat: ensure LoadInviteController calls LoadInvite with correct params

--- src/presentation/controllers/invite/load-invite-controller.ts
+++ src/presentation/controllers/invite/load-invite-controller.ts
@@ -1,11 +1,16 @@
+import { LoadInvite } from '@/domain/usecases'
 import { badRequest } from '@/presentation/helpers'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class LoadInviteController implements Controller {
-  constructor(private readonly _validation: Validation) {}
+  constructor(
+    private readonly _validation: Validation,
+    private readonly _loadInvite: LoadInvite
+  ) {}
   async handle(request: LoadInviteControllerParams): Promise<HttpResponse> {
     const error = this._validation.validate(request)
     if (error) return badRequest(error)
+    await this._loadInvite.load(request)
     return new Promise(resolve => resolve({ statusCode: 200, body: null }))
   }
 }
