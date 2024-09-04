0ac1a5ad0fcff3344388cb0f695909643d302985
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-04 06:07:33 -0700

    feat: add interface LoadInvite to doamin layer

---
+++ src/domain/usecases/invite/load-invite.ts
@@ -0,0 +1,17 @@
+export interface LoadInvite {
+  load: (_code: LoadInviteParams) => Promise<LoadInviteResult>
+}
+
+export type LoadInviteParams = {
+  code: string
+}
+
+export type LoadInviteResult = {
+  inviteId: string
+  inviteCode: string
+  emailUser: string
+  phoneUser: string
+  status: string
+  expiration: Date
+  usedAt: Date | null
+} | null
