e52fdbd6f0ee175a67978c179267619c4ad9a22a
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-04 16:46:06 -0700

    test: ensure UpdateInviteController calls UpdateInvite with correct values

--- tests/presentation/controllers/invite/update-invite-controller.spec.ts
+++ tests/presentation/controllers/invite/update-invite-controller.spec.ts
@@ -1,32 +1,45 @@
 import { faker } from '@faker-js/faker'
 import { ValidationSpy } from '@/tests/presentation/mocks'
-import { LoadInviteSpy } from '@/tests/domain/mocks'
+import { LoadInviteSpy, UpdateInviteSpy } from '@/tests/domain/mocks'
 import { UpdateInviteController, UpdateInviteControllerParams } from '@/presentation/controllers'
 import { MissingParamError, NotFoundError } from '@/presentation/errors'
 import { badRequest, notFound } from '@/presentation/helpers'
 
 const mockRequest = (): UpdateInviteControllerParams => ({
   inviteCode: faker.string.uuid(),
-  status: faker.word.sample(),
+  status: 'used',
   createdAt: faker.date.past().toISOString(),
   expiration: faker.date.future().toISOString(),
   usedAt: faker.date.recent().toISOString()
+})
+
+const mockInviteResult = (): any => ({
+  inviteId: 'any_invite_id',
+  accountId: 'any_account_id',
+  inviteCode: 'any_invite_code',
+  emailUser: 'any_email@user.com',
+  phoneUser: '1234567890',
+  status: 'active',
+  expiration: new Date('2025-01-29T01:29:12.841Z'),
+  usedAt: null
 })
 
 type SutTypes = {
   sut: UpdateInviteController
   validationSpy: ValidationSpy
   loadInviteSpy: LoadInviteSpy
+  updateInviteSpy: UpdateInviteSpy
 }
 
 const makeSut = (): SutTypes => {
   const validationSpy = new ValidationSpy()
   const loadInviteSpy = new LoadInviteSpy()
-  const sut = new UpdateInviteController(validationSpy, loadInviteSpy)
-  return { sut, validationSpy, loadInviteSpy }
+  const updateInviteSpy = new UpdateInviteSpy()
+  const sut = new UpdateInviteController(validationSpy, loadInviteSpy, updateInviteSpy)
+  return { sut, validationSpy, loadInviteSpy, updateInviteSpy }
 }
 
-describe('CreateInvite Controller', () => {
+describe('UpdateInvite Controller', () => {
   it('should call Validation with correct values', async () => {
     const { sut, validationSpy } = makeSut()
     const request = mockRequest()
@@ -34,7 +47,7 @@ describe('CreateInvite Controller', () => {
     expect(validationSpy.input).toEqual(request)
   })
 
-  it('should returns 400 if Validation fails', async () => {
+  it('should return 400 if Validation fails', async () => {
     const { sut, validationSpy } = makeSut()
     validationSpy.error = new MissingParamError(faker.lorem.word())
     const request = mockRequest()
@@ -47,14 +60,31 @@ describe('CreateInvite Controller', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(loadInviteSpy.callsCount).toBe(1)
-    expect(loadInviteSpy.params).toEqual(request)
+    expect(loadInviteSpy.params).toEqual({ inviteCode: request.inviteCode })
   })
 
-  it('should returns 404 if LoadInvite returns null', async () => {
+  it('should return 404 if LoadInvite returns null', async () => {
     const { sut, loadInviteSpy } = makeSut()
     jest.spyOn(loadInviteSpy, 'load').mockResolvedValueOnce(null)
     const request = mockRequest()
     const httpResponse = await sut.handle(request)
     expect(httpResponse).toEqual(notFound(new NotFoundError()))
+  })
+
+  it('should call UpdateInvite with correct values', async () => {
+    const { sut, loadInviteSpy, updateInviteSpy } = makeSut()
+    const inviteData = mockInviteResult()
+    jest.spyOn(loadInviteSpy, 'load').mockResolvedValueOnce(inviteData)
+    const request = mockRequest()
+    await sut.handle(request)
+    const expectedUpdateData = {
+      ...inviteData,
+      inviteCode: request.inviteCode,
+      status: request.status,
+      createdAt: new Date(request.createdAt),
+      expiration: new Date(request.expiration),
+      usedAt: new Date()
+    }
+    expect(updateInviteSpy.updateParams).toEqual(expectedUpdateData)
   })
 })
