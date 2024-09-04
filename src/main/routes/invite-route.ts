b040e0a4a9df2b04fa528b7ac0eaba9abef37e2c
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-04 10:24:03 -0700

    feat: add route /invites/result

--- src/main/routes/invite-route.ts
+++ src/main/routes/invite-route.ts
@@ -2,8 +2,10 @@ import { adaptRoute } from '@/main/adapters'
 
 import { Router } from 'express'
 import { makeCreateInviteController } from '@/main/factories/controllers/invite'
-import { adminAuth } from '../middlewares'
+import { adminAuth } from '@/main/middlewares'
+import { makeLoadInviteController } from '../factories/controllers/invite/load-invite-controller-factory'
 
 export default (router: Router): void => {
   router.post('/invites', adminAuth, adaptRoute(makeCreateInviteController()))
+  router.post('/invites/result', adminAuth, adaptRoute(makeLoadInviteController()))
 }
