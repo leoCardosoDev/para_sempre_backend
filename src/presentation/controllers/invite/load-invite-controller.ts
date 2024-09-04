1351139d592fe726eb4bddcb16b694392b84283e
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-04 09:13:37 -0700

    feat: ensure LoadInviteController return 200 on success

--- src/presentation/controllers/invite/load-invite-controller.ts
+++ src/presentation/controllers/invite/load-invite-controller.ts
@@ -1,6 +1,6 @@
 import { LoadInvite } from '@/domain/usecases'
 import { NotFoundError } from '@/presentation/errors'
-import { badRequest, notFound, serverError } from '@/presentation/helpers'
+import { badRequest, notFound, ok, serverError } from '@/presentation/helpers'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class LoadInviteController implements Controller {
@@ -14,7 +14,7 @@ export class LoadInviteController implements Controller {
       if (error) return badRequest(error)
       const invite = await this._loadInvite.load(request)
       if (!invite) return notFound(new NotFoundError())
-      return new Promise(resolve => resolve({ statusCode: 200, body: null }))
+      return ok(invite)
     } catch (error) {
       return serverError(error as Error)
     }
