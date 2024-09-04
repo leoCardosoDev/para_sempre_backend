8c60850f943a5b585bcb78ce73d3a8411577f4ae
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-04 09:22:39 -0700

    refactor: organize imports

---
+++ src/main/factories/usecases/account/index.ts
@@ -0,0 +1,3 @@
+export * from './authentication-factory'
+export * from './create-account-factory'
+export * from './load-account-by-token-factory'
--- src/main/factories/usecases/index.ts
+++ src/main/factories/usecases/index.ts
@@ -1,4 +1,2 @@
-export * from './authentication-factory'
-export * from './create-account-factory'
-export * from './create-invite-factory'
-export * from './load-account-by-token-factory'
+export * from './account'
+export * from './invite'
