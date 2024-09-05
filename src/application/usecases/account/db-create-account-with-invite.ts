0609222e43f6ad82667723ea419c79f684019805
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-05 12:02:18 -0700

    feat: ensure DbCreateAccountWithInvite calls CheckEmailRepository with correct values

--- src/application/usecases/account/db-create-account-with-invite.ts
+++ src/application/usecases/account/db-create-account-with-invite.ts
@@ -1,12 +1,16 @@
-import { LoadInviteByCodeRepository } from '@/application/protocols'
+import { CheckEmailRepository, LoadInviteByCodeRepository } from '@/application/protocols'
 import { CreateAccountWithInvite, CreateAccountWithInviteParams, CreateAccountWithInviteResult } from '@/domain/usecases'
 
 export class DbCreateAccountWithInvite implements CreateAccountWithInvite {
-  constructor(private readonly _loadInviteByCodeRepository: LoadInviteByCodeRepository) {}
+  constructor(
+    private readonly _loadInviteByCodeRepository: LoadInviteByCodeRepository,
+    private readonly _checkEmailRepository: CheckEmailRepository
+  ) {}
   async create(accountData: CreateAccountWithInviteParams): Promise<CreateAccountWithInviteResult> {
     const invite = await this._loadInviteByCodeRepository.loadByCode(accountData.inviteCode)
     if (!invite || invite.status !== 'active') return { success: false, error: 'Invalid or used invite code' }
     if (invite.emailUser !== accountData.email) return { success: false, error: 'Email does not match the invite' }
+    await this._checkEmailRepository.checkByEmail(accountData.email)
     return { success: true }
   }
 }
