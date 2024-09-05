9981aac71b51bff67fd2f693b33de24eef59bfbc
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-05 12:02:47 -0700

    test: ensure DbCreateAccountWithInvite calls CheckEmailRepository with correct values

--- tests/application/usecases/account/db-create-account-with-invite.spec.ts
+++ tests/application/usecases/account/db-create-account-with-invite.spec.ts
@@ -1,6 +1,6 @@
 import { DbCreateAccountWithInvite } from '@/application/usecases'
 import { mockAccountWithInviteParams, throwError } from '@/tests/domain/mocks'
-import { LoadInviteByCodeRepositorySpy } from '@/tests/application/mocks'
+import { CheckEmailRepositorySpy, LoadInviteByCodeRepositorySpy } from '@/tests/application/mocks'
 
 const mockInviteResult = () => ({
   inviteId: 'any_invite_id',
@@ -10,18 +10,20 @@ const mockInviteResult = () => ({
   phoneUser: '1234567890',
   status: 'active',
   expiration: new Date('2025-01-29T01:29:12.841Z'),
-  usedAt: new Date('2025-01-29T01:29:12.841Z')
+  usedAt: null
 })
 
 type SutTypes = {
   sut: DbCreateAccountWithInvite
   loadInviteByCodeRepository: LoadInviteByCodeRepositorySpy
+  checkEmailRepositorySpy: CheckEmailRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
   const loadInviteByCodeRepository = new LoadInviteByCodeRepositorySpy()
-  const sut = new DbCreateAccountWithInvite(loadInviteByCodeRepository)
-  return { sut, loadInviteByCodeRepository }
+  const checkEmailRepositorySpy = new CheckEmailRepositorySpy()
+  const sut = new DbCreateAccountWithInvite(loadInviteByCodeRepository, checkEmailRepositorySpy)
+  return { sut, loadInviteByCodeRepository, checkEmailRepositorySpy }
 }
 
 describe('DbCreateAccountWithInvite Usecases', () => {
@@ -64,5 +66,15 @@ describe('DbCreateAccountWithInvite Usecases', () => {
     const addParams = mockAccountWithInviteParams()
     const result = await sut.create(addParams)
     expect(result).toEqual({ success: false, error: 'Email does not match the invite' })
+  })
+
+  it('should call CheckEmailRepository with correct values', async () => {
+    const { sut, loadInviteByCodeRepository, checkEmailRepositorySpy } = makeSut()
+    const addParams = { ...mockAccountWithInviteParams(), email: 'same_email@mail.com' }
+    jest.spyOn(loadInviteByCodeRepository, 'loadByCode').mockResolvedValueOnce({ ...mockInviteResult(), emailUser: 'same_email@mail.com' })
+    const checkByEmailSpy = jest.spyOn(checkEmailRepositorySpy, 'checkByEmail')
+    await sut.create(addParams)
+    expect(checkByEmailSpy).toHaveBeenCalledWith(addParams.email)
+    expect(checkByEmailSpy).toHaveBeenCalledTimes(1)
   })
 })
