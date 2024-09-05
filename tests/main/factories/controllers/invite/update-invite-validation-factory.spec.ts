b558e4c9e273bf4747ea69f0ba4749bb523fd0c6
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-04 17:29:08 -0700

    test: ensure makeValidation compositon use validation

---
+++ tests/main/factories/controllers/invite/update-invite-validation-factory.spec.ts
@@ -0,0 +1,16 @@
+import { ValidationComposite, RequiredFieldValidation } from '@/validation/validators'
+import { Validation } from '@/presentation/protocols'
+import { makeUpdateInviteValidation } from '@/main/factories/controllers/invite'
+
+jest.mock('@/validation/validators/validation-composite')
+
+describe('LoadInviteValidation Factory', () => {
+  it('Should call ValidationComposite with all validations', () => {
+    makeUpdateInviteValidation()
+    const validations: Validation[] = []
+    for (const field of ['inviteCode', 'status', 'createdAt', 'expiration', 'usedAt']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
