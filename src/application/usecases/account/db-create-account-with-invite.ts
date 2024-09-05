70c2fa7a2256761179b0777fb1f73b0ce2bec203
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-05 11:21:17 -0700

    feat: ensure DbCreateAccountWithInvite return false if email does match

--- src/application/usecases/account/db-create-account-with-invite.ts
+++ src/application/usecases/account/db-create-account-with-invite.ts
@@ -6,6 +6,7 @@ export class DbCreateAccountWithInvite implements CreateAccountWithInvite {
   async create(accountData: CreateAccountWithInviteParams): Promise<CreateAccountWithInviteResult> {
     const invite = await this._loadInviteByCodeRepository.loadByCode(accountData.inviteCode)
     if (!invite || invite.status !== 'active') return { success: false, error: 'Invalid or used invite code' }
+    if (invite.emailUser !== accountData.email) return { success: false, error: 'Email does not match the invite' }
     return { success: true }
   }
 }
