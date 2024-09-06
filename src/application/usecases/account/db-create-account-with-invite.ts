533c88513d085aaa0204f63477f2e0a5992ca532
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-06 07:49:25 -0700

    feat: ensure DbCreateAccountWithInvite calls CreateAccoutRepository with correct values

--- src/application/usecases/account/db-create-account-with-invite.ts
+++ src/application/usecases/account/db-create-account-with-invite.ts
@@ -1,11 +1,12 @@
-import { CheckEmailRepository, Hasher, LoadInviteByCodeRepository } from '@/application/protocols'
+import { CheckEmailRepository, CreateAccountWithInviteRepository, Hasher, LoadInviteByCodeRepository } from '@/application/protocols'
 import { CreateAccountWithInvite, CreateAccountWithInviteParams, CreateAccountWithInviteResult } from '@/domain/usecases'
 
 export class DbCreateAccountWithInvite implements CreateAccountWithInvite {
   constructor(
     private readonly _loadInviteByCodeRepository: LoadInviteByCodeRepository,
     private readonly _checkEmailRepository: CheckEmailRepository,
-    private readonly _hasher: Hasher
+    private readonly _hasher: Hasher,
+    private readonly _createAccountWithInviteRepository: CreateAccountWithInviteRepository
   ) {}
   async create(accountData: CreateAccountWithInviteParams): Promise<CreateAccountWithInviteResult> {
     const invite = await this._loadInviteByCodeRepository.loadByCode(accountData.inviteCode)
@@ -14,7 +15,8 @@ export class DbCreateAccountWithInvite implements CreateAccountWithInvite {
     if (invite.emailUser !== accountData.email) return { success: false, error: 'Email does not match the invite' }
     const emailExist = await this._checkEmailRepository.checkByEmail(accountData.email)
     if (emailExist) return { success: false, error: 'Email already registered' }
-    await this._hasher.hash(accountData.password)
+    const hashedPassword = await this._hasher.hash(accountData.password)
+    await this._createAccountWithInviteRepository.create({ ...accountData, password: hashedPassword })
     return { success: true }
   }
 }
