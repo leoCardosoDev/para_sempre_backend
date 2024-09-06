aa2867f7ba8a6e19e20284660d1696665506b583
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-06 06:51:31 -0700

    feat: ensure DbCreateAccountWithInvite calls Hasher with correct values

--- src/application/usecases/account/db-create-account-with-invite.ts
+++ src/application/usecases/account/db-create-account-with-invite.ts
@@ -1,10 +1,11 @@
-import { CheckEmailRepository, LoadInviteByCodeRepository } from '@/application/protocols'
+import { CheckEmailRepository, Hasher, LoadInviteByCodeRepository } from '@/application/protocols'
 import { CreateAccountWithInvite, CreateAccountWithInviteParams, CreateAccountWithInviteResult } from '@/domain/usecases'
 
 export class DbCreateAccountWithInvite implements CreateAccountWithInvite {
   constructor(
     private readonly _loadInviteByCodeRepository: LoadInviteByCodeRepository,
-    private readonly _checkEmailRepository: CheckEmailRepository
+    private readonly _checkEmailRepository: CheckEmailRepository,
+    private readonly _hasher: Hasher
   ) {}
   async create(accountData: CreateAccountWithInviteParams): Promise<CreateAccountWithInviteResult> {
     const invite = await this._loadInviteByCodeRepository.loadByCode(accountData.inviteCode)
@@ -12,6 +13,7 @@ export class DbCreateAccountWithInvite implements CreateAccountWithInvite {
     if (invite.emailUser !== accountData.email) return { success: false, error: 'Email does not match the invite' }
     const emailExist = await this._checkEmailRepository.checkByEmail(accountData.email)
     if (emailExist) return { success: false, error: 'Email already registered' }
+    await this._hasher.hash(accountData.password)
     return { success: true }
   }
 }
